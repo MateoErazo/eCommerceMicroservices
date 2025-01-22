cfb822da2ac8789b25893dcd13348dcba256e8b6
Jesús Mateo <erazojesusmateo@gmail.com>
2025-01-22 14:15:43 -0500

    I add dapper and implementations of select and insert reposiroties methods

--- eCommerceSolution.UsersService/eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.DbContext;
 using eCommerce.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +17,8 @@ public static class DependencyInjection
     //TO DO: Add services to the IoC container
     //Infrastructure services often include data access, caching and other low-level components.
     services.AddTransient<IUsersRepository, UsersRepository>();
+
+    services.AddTransient<DapperDbContext>();
     return services;
   }
 }
--- eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -1,29 +1,47 @@
-﻿using eCommerce.Core.DTO;
+﻿using Dapper;
+using eCommerce.Core.DTO;
 using eCommerce.Core.Entities;
 using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.DbContext;
 
 namespace eCommerce.Infrastructure.Repositories
 {
   internal class UsersRepository : IUsersRepository
   {
+    private readonly DapperDbContext _dbContext;
+
+    public UsersRepository(DapperDbContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
     public async Task<ApplicationUser?> AddUser(ApplicationUser user)
     {
       //generate a new unique User Id for the user
       user.UserId = Guid.NewGuid();
+
+      //SQL Query to insert data into the "Users" table.
+      string query = "INSERT INTO public.\"Users\"(\"UserId\",\"Email\",\"PersonName\",\"Gender\",\"Password\") VALUES (@UserId, @Email, @PersonName, @Gender, @Password)";
+
+      int rowsCountAffected = await _dbContext.DbConnection.ExecuteAsync(query, user);
+
+      if (rowsCountAffected <= 0) {
+        return null;
+      }
 
       return user;
     }
 
     public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
     {
-      return new ApplicationUser()
-      {
-        UserId = Guid.NewGuid(),
-        Email = email,
-        Password = password,
-        PersonName = "Person name",
-        Gender = GenderOptions.Male.ToString()
-      };
+      //SQL Query to select a user by Email and Password.
+      string query = "SELECT * FROM public.\"Users\" WHERE \"Email\"=@Email AND \"Password\"=@Password";
+      
+      ApplicationUser? user = await _dbContext.DbConnection
+        .QueryFirstOrDefaultAsync<ApplicationUser>(query, new {Email=email, Password=password});
+
+      if (user == null) { return null; }
+
+      return user;
     }
   }
 }
