2ecb55a13baf1c2f1706f47ad7982cd85f7d86ed
Jesús Mateo <erazojesusmateo@gmail.com>
2025-03-06 16:46:33 -0500

    I added register request validator

---
+++ eCommerceSolution.UsersService/eCommerce.Core/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Core.DTO;
+using FluentValidation;
+
+namespace eCommerce.Core.Validators;
+
+public class RegisterRequestValidator: AbstractValidator<RegisterRequest>
+{
+  public RegisterRequestValidator()
+  {
+    RuleFor(temp => temp.Email)
+      .NotEmpty().WithMessage("The field email is required")
+      .EmailAddress().WithMessage("Invalid email address format");
+
+    RuleFor(temp => temp.Password)
+      .NotEmpty().WithMessage("The field password is required");
+
+    RuleFor(temp => temp.PersonName)
+      .NotEmpty().WithMessage("The field personName is required");
+
+    RuleFor(temp => temp.Gender)
+      .NotEmpty().WithMessage("The field gender is required")
+      .IsInEnum().WithMessage("Invalid gender value");
+  }
+}
